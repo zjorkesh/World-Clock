7a7501190fe3b84986718c794e55d27bc0915f31
Zeinab <z.jorkesh1989@gmail.com>
2026-07-07 17:17:50 +0800

    Refactored HTML structure and created two-column page layout

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-function updateCityTime(id,timeZone){
-    let cityElement = document.querySelector(id);
-    let timeElement = cityElement.querySelector(".time");
-    let dateElement = cityElement.querySelector(".date");
+function updateCityTime(id, timeZone) {
+  let cityElement = document.querySelector(id);
+  let timeElement = cityElement.querySelector(".time");
+  let dateElement = cityElement.querySelector(".date");
 
-    let cityTime = moment().tz(timeZone);
+  let cityTime = moment().tz(timeZone);
 
-    dateElement.innerHTML = cityTime.format("MMMM Do YYYY")
-    timeElement.innerHTML = `${cityTime.format("h:mm:ss")} <span class="period">${cityTime.format("A")}</span>`;
+  dateElement.innerHTML = cityTime.format("MMMM Do YYYY");
+  timeElement.innerHTML = `${cityTime.format("h:mm:ss")} <span class="period">${cityTime.format("A")}</span>`;
 }
 
 function updateTime() {
@@ -15,29 +15,28 @@ function updateTime() {
   updateCityTime("#tokyo", "Asia/Tokyo");
 }
 
-function updateCity(event){
-    let timeZone = event.target.value;
-    if (timeZone==='current'){
-        timeZone = moment.tz.guess();
-    }
-    if (timeZone.length === 0) {
-        return;
-    }
-    let cityName = timeZone.split("/")[1].replaceAll("_", " ");
-    let cityTime = moment().tz(timeZone);
+function updateCity(event) {
+  let timeZone = event.target.value;
+  if (timeZone === "current") {
+    timeZone = moment.tz.guess();
+  }
+  if (timeZone.length === 0) {
+    return;
+  }
+  let cityName = timeZone.split("/")[1].replaceAll("_", " ");
+  let cityTime = moment().tz(timeZone);
 
-    let citiesElement = document.querySelector(".cities");
-    
-    citiesElement.innerHTML =`
+  let citiesElement = document.querySelector(".cities");
+
+  citiesElement.innerHTML = `
             <div class="city">
             <h2><span >${cityName} </span> <span class="time">${cityTime.format("h:mm:ss")}<span class="period"> ${cityTime.format("A")}</span></span></h2>
             <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
         </div>`;
 }
- 
-function displayTimeZone(timezone){
 
-    selectElement.innerHTML +=`
+function displayTimeZone(timezone) {
+  selectElement.innerHTML += `
     <option value ="${timezone}">${timezone}</option>`;
 }
 
@@ -49,4 +48,3 @@ timezones.forEach(displayTimeZone);
 
 updateTime();
 setInterval(updateTime, 1000);
-
